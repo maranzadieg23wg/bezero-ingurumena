c94b89213b9f883c53777ed4394aab91d7554b89
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-09-17 14:00:04 +0200

    pasahitza

--- Ariketak/3. Objetuak erabiltzen/2.4/3. Number/3. Pasahitza/main.js
+++ Ariketak/3. Objetuak erabiltzen/2.4/3. Number/3. Pasahitza/main.js
@@ -1,3 +1,16 @@
 function start(){
     let pasahitza = prompt("Sartu zure pasahitza");
+
+}
+
+
+
+function balidatu(cadena) {
+    const zenbakiaDauka = /\d/;          // Expresión regular para encontrar un número
+    const maiuskula = /[A-Z]/;    // Expresión regular para encontrar una letra mayúscula
+    const minuskula = /[a-z]/;    // Expresión regular para encontrar una letra minúscula
+
+    return zenbakiaDauka.test(cadena) && 
+            maiuskula.test(cadena) && 
+            minuskula.test(cadena);
 }
