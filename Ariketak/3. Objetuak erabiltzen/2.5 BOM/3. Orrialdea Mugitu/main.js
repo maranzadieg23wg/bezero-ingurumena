9884aeb9ef382e38b660f183a71a364254d98d92
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-09-22 12:19:17 +0200

    Leioa mugitzen da

--- Ariketak/3. Objetuak erabiltzen/2.5 BOM/3. Orrialdea Mugitu/main.js
+++ Ariketak/3. Objetuak erabiltzen/2.5 BOM/3. Orrialdea Mugitu/main.js
@@ -4,4 +4,18 @@ function leioBerria(){
     newWin = window.open("", 200, 200);
 }
 
+function gora(){
+    newWin.moveBy(0,-10)
+}
 
+function bera(){
+    newWin.moveBy(0,10)
+}
+
+function eskubi(){
+    newWin.moveBy(10,0)
+}
+
+function ezker(){
+    newWin.moveBy(-10,0)
+}
