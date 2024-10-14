91ef4d3c110b1d8fb7718a329dc65ee5881b4ed9
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-10-14 19:11:07 +0200

    ariketa eginda

--- Ariketak/5.1 Gertaerak eta inprimakiak/3. Formulario/4. Formulario/main.js
+++ Ariketak/5.1 Gertaerak eta inprimakiak/3. Formulario/4. Formulario/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
     let datuak = document.querySelectorAll('.datuak');
-
     
     if (datuak.length > 0) {
         datuak[0].focus();
@@ -20,6 +20,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 
             }
+
+            for(let i = index; i >= 0; i--) {
+                if (datuak[i].value === "") {
+                    datuak[i].classList.add("error");
+                } else {
+                    datuak[i].classList.remove("error");
+                }
+            }
+
+           
         }
+        
+        
     });
 });
+
+
+
+function garbitu(){
+    let datuak = document.querySelectorAll('.datuak');
+    for(let i=0;i<datuak.length;i++){
+        
+        datuak[i].value = "";
+        console.log("garbitu da");
+    }
+}
