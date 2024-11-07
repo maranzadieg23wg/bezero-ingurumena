737f2bc618f98fc4617f9e66d6c5d839bb25600e
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-11-07 09:40:00 +0000

    Eginda

--- Ariketak/7. Komunikazio asinkronoa/2.9 ARIKETA - AJAX - Sarrera/2 Ariketa/cliente/main.js
+++ Ariketak/7. Komunikazio asinkronoa/2.9 ARIKETA - AJAX - Sarrera/2 Ariketa/cliente/main.js
@@ -1,30 +1,67 @@
 let lista =[];
-let url = "http://php-fpm:8080/generaContenidos.php" //http://10.14.0.2:8080/generaContenidos.php
+//let url = "http://php-fpm/generaContenidos.php"; //http://10.14.0.2:8080/generaContenidos.php
+let url = "http://10.14.0.2:8080/generaContenidos.php";
+
+let jarraitu = true;
+
+let pos = 0;
 
 function siguiente(){
+    if(lista[pos+1] !==undefined){
+        pos++;
+        idatzi(lista[pos]);
+    }
 
 }
 
 function anterior(){
+    if(lista[pos-1] !==undefined){
+        pos--;
+        idatzi(lista[pos]);
+    }
     
 }
 
 function detener(){
-    
+    jarraitu = false;
+    clearInterval(temo);
 }
 
 
 function lortuDatuak(){
+
+    
     let http = new XMLHttpRequest();
     http.onreadystatechange = function(){
         
 
-
+       
         
-        console.log(this.responseText);
+        if (this.readyState === 4 && this.status === 200) {
+            console.log("Respuesta del servidor:", this.responseText);
+            lista.push(this.responseText);
+            idatzi(this.responseText);
+            pos++;
+            
+        } else if (this.readyState === 4) {
+            // Manejar errores
+            console.error("Error en la solicitud:", this.status, this.statusText);
+        }else{
+            console.error("Ha petado por todo los lados ")
+        }
 
     };
 
-    http.open('GET', url);
+    http.open('GET', url, true);
     http.send();
+}
+let temo;
+if(jarraitu){
+    temo = setInterval(lortuDatuak, 1000);
+}
+
+
+function idatzi (text){
+
+    document.getElementById("ticker").innerHTML = text;
 }
--- Ariketak/7. Komunikazio asinkronoa/2.9 ARIKETA - AJAX - Sarrera/2 Ariketa/server/src/generaContenidos.php
+++ Ariketak/7. Komunikazio asinkronoa/2.9 ARIKETA - AJAX - Sarrera/2 Ariketa/server/src/generaContenidos.php
@@ -1,27 +1,39 @@
 <?php
-// Frases de contenido de las que se selecciona aleatoriamente una de ellas
-$frases = array("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-                "Integer augue. Maecenas vel arcu.",
-                "Nunc ipsum enim, pretium vel, blandit quis, rutrum sit amet, mauris.",
-                "Proin at lectus mattis elit facilisis aliquam.",
-                "Sed accumsan, neque in luctus egestas, ipsum enim hendrerit massa, at venenatis felis purus sed velit.",
-                "Mauris sit amet magna ut leo ullamcorper semper.",
-                "Vivamus venenatis consequat massa.",
-                "Praesent erat justo, varius eu, porta at, sollicitudin in, nibh.",
-                "Aenean ligula ante, vehicula sit amet, vestibulum eu, mattis non, mauris.",
-                "Ut suscipit fermentum eros. Nullam ut mauris vel eros placerat cursus.",
-                "Praesent non lectus. Nam condimentum nisi nec felis.",
-                "Sed tincidunt ullamcorper pede.",
-                "Curabitur facilisis, quam sed egestas tincidunt, justo nisl tempor odio, non auctor metus sapien sed risus.",
-                "Fusce risus lectus, consequat at, molestie sit amet, auctor tristique, massa.",
-                "Mauris vitae nisi. Aliquam tempus.",
-                "Etiam eu neque sit amet nibh tempus lacinia.",
-                "Nunc rutrum lorem eu erat. Proin ac felis. Fusce commodo leo.");
+// Agregar las cabeceras CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// Generar un número aleatorio comprendido entre 0 y el número total de frases disponibles
+// Responder a las solicitudes OPTIONS y detener el script
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
+// Frases de contenido de las que se selecciona aleatoriamente una de ellas
+$frases = array(
+    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    "Integer augue. Maecenas vel arcu.",
+    "Nunc ipsum enim, pretium vel, blandit quis, rutrum sit amet, mauris.",
+    "Proin at lectus mattis elit facilisis aliquam.",
+    "Sed accumsan, neque in luctus egestas, ipsum enim hendrerit massa, at venenatis felis purus sed velit.",
+    "Mauris sit amet magna ut leo ullamcorper semper.",
+    "Vivamus venenatis consequat massa.",
+    "Praesent erat justo, varius eu, porta at, sollicitudin in, nibh.",
+    "Aenean ligula ante, vehicula sit amet, vestibulum eu, mattis non, mauris.",
+    "Ut suscipit fermentum eros. Nullam ut mauris vel eros placerat cursus.",
+    "Praesent non lectus. Nam condimentum nisi nec felis.",
+    "Sed tincidunt ullamcorper pede.",
+    "Curabitur facilisis, quam sed egestas tincidunt, justo nisl tempor odio, non auctor metus sapien sed risus.",
+    "Fusce risus lectus, consequat at, molestie sit amet, auctor tristique, massa.",
+    "Mauris vitae nisi. Aliquam tempus.",
+    "Etiam eu neque sit amet nibh tempus lacinia.",
+    "Nunc rutrum lorem eu erat. Proin ac felis. Fusce commodo leo."
+);
+
+// Generar un nÃºmero aleatorio para seleccionar una frase
 srand((double)microtime()*1000000);
 $numeroAleatorio = rand(0, count($frases)-1);
 
-// La respuesta de este script consiste en una frase seleccionada aleatoriamente
+// La respuesta del script es una frase seleccionada aleatoriamente
 echo $frases[$numeroAleatorio];
 ?>
