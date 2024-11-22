f30e62d3752679ed6edd4790bb722c57ba15c686
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-11-22 08:05:54 +0100

    Azterketako fitxategiak deskargatuta

---
+++ Azterketak/2. Azterketa/client/examen_ej1_script.js
@@ -0,0 +1,19 @@
+// inicialización (1 punto)
+function inicio() {
+}
+
+// carga de datos desde servidor local (1 puntos)
+function cargaDatos() {
+}
+
+// explotación de datos recibidos (1 punto)
+function cargaMunicipios() {
+}
+
+// explotación de datos recibidos (1 punto)
+function cargaRetaurantes() {
+}
+
+// explotación de datos recibidos (1 punto)
+function cargaInformacion() {
+}
---
+++ Azterketak/2. Azterketa/client/examen_ej2_script.js
@@ -0,0 +1,19 @@
+// inicialización (0,5 punto)
+function inicio (){
+}
+
+// carga de datos desde servidor local (1 puntos)
+function cargaForecast() {
+}
+
+// explotación de datos recibidos (1,5 punto)
+function muestraForecast() {
+}
+
+// explotación de datos recibidos (1,5 punto)
+function infoForecast () {
+}
+
+// visibilidad apartado información (0,5 punto)
+function muestra_esconde() {
+}
